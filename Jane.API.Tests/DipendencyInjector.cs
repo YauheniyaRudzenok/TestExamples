59bcbafd8fce48d5fef6211c708bfa0ac6330cc9
YauheniyaRudzeok <yauheniyarudzenok@gmail.com>
2020-12-03 14:59:48 +0100

    fix uncommitted items

---
+++ Jane.API.Tests/DipendencyInjector.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using NLog.Extensions.Logging;
+
+namespace Jane.API.Tests
+{
+    public static class DI
+    {
+        private static IServiceProvider BuildDi(IConfiguration config)
+        {
+            return new ServiceCollection()
+               .AddTransient<Runner>() // Runner is the custom class
+               .AddLogging(loggingBuilder =>
+               {
+               // configure Logging with NLog
+               loggingBuilder.ClearProviders();
+                   loggingBuilder.SetMinimumLevel(Microsoft.Extensions.Logging.LogLevel.Trace);
+                   loggingBuilder.AddNLog(config);
+               })
+               .BuildServiceProvider();
+        }
+    }
+}
---
+++ Jane.API.Tests/Logger.cs
@@ -0,0 +1,14 @@
+﻿using NLog;
+using RestSharp;
+
+namespace Jane.API.Tests
+{
+    public static class Log
+    {
+        public static void TestTitle(Logger log, string title) => log.Info(title.ToUpper);
+        public static void StatusCode(Logger log, IRestResponse response, string expected) =>
+            log.Info("Response code is: {0}, Expected: {1}", response.StatusCode, expected);
+
+        public static void ClientCreation(Logger log) => log.Info("Client is created");
+    }
+}
